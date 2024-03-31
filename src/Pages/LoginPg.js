cf9f33a67c31c2a02cdb85d06442bc095ac1f4a4
Anjali <np03cs4a220323@heraldcollege.edu.np>
2024-03-31 23:37:04 +0545

    add necessary icon fo login

--- src/Pages/LoginPg.js
+++ src/Pages/LoginPg.js
@@ -1,4 +1,9 @@
 import React from "react";
+import "../styles/LoginPg.css";
+import PhoneIcon from "@mui/icons-material/Phone";
+import HomeIcon from "@mui/icons-material/Home";
+import EmailIcon from "@mui/icons-material/Email";
+import LockIcon from "@mui/icons-material/Lock";
 
 function LoginPg() {
   return <div>LoginPg</div>;
