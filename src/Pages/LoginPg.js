e2d3d8e008b2fbeedab853e7440ceb9d96517eca
Anjali <np03cs4a220323@heraldcollege.edu.np>
2024-03-31 23:38:10 +0545

    add missing icon

--- src/Pages/LoginPg.js
+++ src/Pages/LoginPg.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/LoginPg.css";
+import BadgeIcon from "@mui/icons-material/Badge";
 import PhoneIcon from "@mui/icons-material/Phone";
 import HomeIcon from "@mui/icons-material/Home";
 import EmailIcon from "@mui/icons-material/Email";
