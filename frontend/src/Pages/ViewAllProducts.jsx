dce0be15468a12da5c40280bfeb9f1b67e29eeaf
HeraldMagar <119142885+HeraldMagar@users.noreply.github.com>
2024-05-23 23:01:10 +0545

    search function

--- frontend/src/Pages/ViewAllProducts.jsx
+++ frontend/src/Pages/ViewAllProducts.jsx
@@ -6,12 +6,13 @@ import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import ProductCard from "../Components/productCard";
 import "../styles/viewAllcategory.css";
-import Search from "../Components/Search";
 import { Box } from "@mui/material";
 
 const ViewAllProducts = () => {
   const { category } = useParams();
   const [products, setProducts] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [isSearching, setIsSearching] = useState(false);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -32,14 +33,73 @@ const ViewAllProducts = () => {
       }
     };
     fetchProducts();
+
+  
   }, [category]);
   console.log("products: ", products);
+  const handleSearch = () => {
+    setIsSearching(true);
+    // Perform search functionality with searchValue
+
+    // search for the text in the product name or description
+    const searchResults = products.filter(
+      (product) =>
+        product.productName.toLowerCase().includes(searchValue.toLowerCase()) ||
+        product.productDescription
+          .toLowerCase()
+          .includes(searchValue.toLowerCase())
+    );
+
+    setProducts(searchResults) // Update the products with the search results
+
+    // Reset the search value
+    setSearchValue("");
+
+    setTimeout(() => {
+      setIsSearching(false);
+    }, 2000);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      handleSearch();
+    }
+  };
 
   return (
     <div className="view-all-container">
       <h1>All {category}s</h1>
       <div className="seperator" />
-      <Search products={products ? products : []} />
+      <div className="home-container">
+      {" "}
+      {/* The search*/}
+      <div className="Search-container">
+        <h1>Order Now!</h1>
+        <div className="searchNbtn">
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="Enter product name"
+              name="search"
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+              onKeyDown={handleKeyPress}
+            />
+          </div>
+          <button onClick={handleSearch}>Search</button>
+        </div>
+      </div>
+      {/* Pop-up card */}
+      {isSearching && (
+        <div className="searching-popup-card">
+          <div className="popup-content">
+            <p>We are searching...</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
       <div className="product-grid">
         <Box sx={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
           {products &&
